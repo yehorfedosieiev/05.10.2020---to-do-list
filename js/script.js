ee6653e04e4b0c259e94dc3532e2c355265b13a6
Yehor Fedosieiev <pifagorka@gmail.com>
2020-10-09 00:32:28 +0300

    05.10.2020 TODO list homework_1. +1 question!

--- js/script.js
+++ js/script.js
@@ -5,12 +5,14 @@ formAddTask.addEventListener('submit', handleFormAdSubmit);
 function handleFormAdSubmit(event){
     event.preventDefault();
 
-    // console.log('this', this);
+    console.log('----------');
+    console.log('this =', this);
     // console.dir(this);
-
-    // console.log(this.querySelector('[name="title"]'));
-
-    // console.log("то что в input", this.querySelector('[name="title"]').value); // то что в input
+    console.log('----------');
+    console.log(this.querySelector('[name="title"]'));
+    console.log('----------');
+    console.log("то что в input =", this.querySelector('[name="title"]').value); // то что в input
+    console.log('----------');
 
     const task = {
         title: this.querySelector('[name="title"]').value,
@@ -24,25 +26,61 @@ function handleFormAdSubmit(event){
 
     $('#modalAddTask').modal('hide');
 
-    this.reset();
+    this.reset(); //метод у формы, чтобы сбросить все значения полей по умолчанию
 }
 
 function addTask(task){
     const taskList = document.querySelector(`[data-status="${task.status}"]`);
-    console.log(`[data-status="${task.status}"]`)
-    console.log(task)
-    
+    console.log(`[data-status="${task.status}"]`);
+    console.log('----------');
+    console.log(task);
+    console.log('----------');
     console.log(taskList);
+    console.log('----------');
 
     const taskListItem = document.createElement("li");
     taskListItem.classList.add('list-group-item');
     taskListItem.innerText = task.title;
     taskList.appendChild(taskListItem);
+    showStatistic ();
 };
 
 for(let key in localStorage){
     if (!localStorage.hasOwnProperty(key)) continue;
     
     const task = JSON.parse(localStorage[key]);
+    console.log(task);
+    console.log('----------');
     addTask(task);
 }
+//-----Homework-----
+
+const removeAllTask = document.querySelector('#removeAllTask');
+
+removeAllTask.addEventListener('click', removeTasks);
+
+function removeTasks(){
+    localStorage.clear();
+    document.querySelector('[data-status]').innerHTML = '';
+    // как найти все элементы и поместить их в массив???
+    // Array.from(document.querySelectorAll('[data-status]').querySelectorAll('li'));
+    showStatistic ();
+}
+
+function showStatistic (){
+    let tasksCount={
+        1: 0,
+        2: 0,
+        3: 0
+    };
+
+    for(let key in localStorage){
+        if (!localStorage.hasOwnProperty(key)) continue;
+        const task = JSON.parse(localStorage[key]);
+        tasksCount[task.status]++;
+    }
+
+    for(let key in tasksCount){
+        document.querySelector(`[data-status-count="${key}"]`).innerText = tasksCount[key];
+    }
+};
